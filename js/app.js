0ec9c4bd401b8970ac2aed41b533eeb2c4d0a182
AbelW <unoctium1@gmail.com>
2021-01-01 10:13:52 -0800

    Replace shader

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "%UNITY_WEBGL_BUILD_URL%");  
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;

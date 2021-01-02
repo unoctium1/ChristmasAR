ae350f848f9a6ba597c564108cddf166222b528a
AbelW <unoctium1@gmail.com>
2021-01-01 21:59:50 -0800

    Update template and minor changes to the scene and scaling

--- Assets/Scripts/StartOnMarkerDetected.cs
+++ Assets/Scripts/StartOnMarkerDetected.cs
@@ -8,6 +8,8 @@ public class StartOnMarkerDetected : MonoBehaviour
 {
     PlayableDirector dir;
 
+    bool isFirstTime = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +21,12 @@ public class StartOnMarkerDetected : MonoBehaviour
 
     void onMarkerVisible(MarkerInfo m)
     {
-        if (dir.state == PlayState.Paused) dir.Resume();
-        else
+        if (isFirstTime)
         {
             dir.Play();
+            isFirstTime = false;
         }
+        else if (dir.state == PlayState.Paused) dir.Resume();
     }
 
     void onMarkerLost(MarkerInfo m)
